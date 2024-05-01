e3a62dd90dde590e1c189fce3f4b59fa558cf0bb
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-04-30 19:09:30 -0600

    dashboard now loads when user is authenticate

--- src/app/dashboard/[userEmail]/page.tsx
+++ src/app/dashboard/[userEmail]/page.tsx
@@ -1,28 +1,44 @@
-'use client';
+"use client";
 import { useState, useEffect } from "react";
 
 type Prop = {
-  userEmail: string;
+  params: {
+    userEmail: string;
+  };
 };
 
-export default function Dashboard (props: Prop) {
-  
-  const userEmail = props.userEmail;
-  const [user, setUser] = useState<JSON>();
+export default function Dashboard(props: Prop) {
+  const userEmail = props.params.userEmail;
+  const decodedUserEmail = decodeURIComponent(userEmail);
+  console.log("Attempted User Email ", decodedUserEmail);
+  const [user, setUser] = useState<string>();
 
   // fetch the user data from the server
-  //Todo: Add the fetch call here
 
-  return (
-    user ? (
-      <div>
-        <h1>Welcome </h1>
-        <p>Good Day</p>
-      </div>
-    ) : (
-      <p>Loading...</p>
-    )
+  useEffect(() => {
+    try {
+      fetch(`/auth/${decodedUserEmail}`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.status === 200) {
+            console.log(data);
+            setUser(data.userEmail);
+          } else {
+            console.log("User not authenticated");
+          }
+        });
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  }, [decodedUserEmail]);
+
+  return user ? (
+    <div>
+      <h1>Welcome </h1>
+      <p>Good Day</p>
+    </div>
+  ) : (
+    <p>Loading...</p>
   );
 }
-
-    
