7a839f83090eb96e71eb850cb5046e3baf681182
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-04-30 18:33:10 -0600

    auth with api works

--- src/app/dashboard/[userEmail]/page.tsx
+++ src/app/dashboard/[userEmail]/page.tsx
@@ -1,8 +1,28 @@
- export default function Dashboard() {
-    return (
-        <div>
-        <h1>Dashboard</h1>
-        <p>Welcome to your dashboard!</p>
-        </div>
-    );
+'use client';
+import { useState, useEffect } from "react";
+
+type Prop = {
+  userEmail: string;
+};
+
+export default function Dashboard (props: Prop) {
+  
+  const userEmail = props.userEmail;
+  const [user, setUser] = useState<JSON>();
+
+  // fetch the user data from the server
+  //Todo: Add the fetch call here
+
+  return (
+    user ? (
+      <div>
+        <h1>Welcome </h1>
+        <p>Good Day</p>
+      </div>
+    ) : (
+      <p>Loading...</p>
+    )
+  );
 }
+
+    
